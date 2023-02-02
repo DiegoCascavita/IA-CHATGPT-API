bb6c46f1065d77392c199db3f7a8a9f2a138282e
DiegoCascavita <sportscenter857@gmail.com>
2023-02-02 18:10:27 -0500

    typing function in js

---
+++ client/script.js
@@ -0,0 +1,46 @@
+import bot from './assets/bog.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element){
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if(element.textContent === '....'){
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text){
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text.charAt(index);
+      index++;
+    }else{
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe = (isAi, value, uniqueId){
+  `
+  <div class="wrapper ">
+  `
+}
