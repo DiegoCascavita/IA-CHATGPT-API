e4dceb804e68a869f12d836b5d5b031f58d299a8
DiegoCascavita <sportscenter857@gmail.com>
2023-02-03 14:19:45 -0500

    gray separator btw user and chat gpt implemented

--- client/script.js
+++ client/script.js
@@ -1,46 +1,65 @@
-import bot from './assets/bog.svg';
-import user from './assets/user.svg';
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-let loadInterval;
+let loadInterval
 
-function loader(element){
-  element.textContent = '';
+function loader(element) {
+    element.textContent = ''
 
-  loadInterval = setInterval(() => {
-    element.textContent += '.';
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
 
-    if(element.textContent === '....'){
-      element.textContent = '';
-    }
-  }, 300)
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
 }
 
-function typeText(element, text){
-  let index = 0;
+function typeText(element, text) {
+    let index = 0
 
-  let interval = setInterval(() => {
-    if(index < text.length){
-      element.innerHTML += text.charAt(index);
-      index++;
-    }else{
-      clearInterval(interval);
-    }
-  }, 20)
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
 }
 
-function generateUniqueId(){
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`;
+    return `id-${timestamp}-${hexadecimalString}`;
 }
 
-function chatStripe = (isAi, value, uniqueId){
-  `
-  <div class="wrapper ">
-  `
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
 }
+
+
