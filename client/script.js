8afce86ce865322d700cfd495edf08983e758f48
DiegoCascavita <sportscenter857@gmail.com>
2023-02-19 11:32:49 -0500

    adding popUp warning

--- client/script.js
+++ client/script.js
@@ -1,6 +1,9 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
+// --------------------------POP UP
+
+// ---------------------------- API 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
