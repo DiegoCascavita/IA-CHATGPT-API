b254923bab90af046fcc60a64b7a81d7a8b18db7
DiegoCascavita <sportscenter857@gmail.com>
2023-04-04 11:48:49 -0500

    fixing api acces

--- server/server.js
+++ server/server.js
@@ -46,3 +46,5 @@ app.post('/', async (req, res) => {
 })
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+
+// be carefull with the api consumption and pricing $0.020 per token
